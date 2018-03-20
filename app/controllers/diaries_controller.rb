794a3d070afbc33b49e0f96c5d07807bc509126b
BIN <bnakamura@binn.biz>
2018-03-20 21:44:00 +0000

    投稿ページの作成

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,5 +1,17 @@
 class DiariesController < ApplicationController
   def index
-    @diaries = Diary.all
+    @diaries = Diary.all.order("created_at DESC")
+  end
+  
+  def new
+  end
+  
+  def create
+    Diary.create(diary_params)
+  end
+  
+  private
+  def diary_params
+    params.permit(:title, :author, :publisher, :public_flag, :impression, :impression_public_flag, :memo, :memo_public_flag, :action, :action_public_flag, :reco_for, :reco_for_public_flag)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get 'diaries' => 'diaries#index' 
+  root 'diaries#index'                   #日記一覧をルートパスに指定する
+  get 'diaries'     => 'diaries#index'   #日記一覧画面（トップページ）
+  get 'diaries/new' => 'diaries#new'     #日記投稿画面
+  post 'diaries'    => 'diaries#create'  #日記投稿機能
 end
