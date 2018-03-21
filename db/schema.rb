7313c3d64328606e52e90cf68b0fc85e2b54a98f
BIN <bnakamura@binn.biz>
2018-03-21 01:56:03 +0000

    Userテーブルにnicknameを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320220810) do
+ActiveRecord::Schema.define(version: 20180321012950) do
 
   create_table "diaries", force: :cascade do |t|
     t.string   "title",                  limit: 255
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180320220810) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
