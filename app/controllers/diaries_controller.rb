7394718f9c7febabe3a99772d2b8cde712b98812
BIN <bnakamura@binn.biz>
2018-03-29 00:56:53 +0000

    日記削除機能の追加

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -13,6 +13,13 @@ class DiariesController < ApplicationController
     Diary.create(title: diary_params[:title], author: diary_params[:author], publisher: diary_params[:publisher], public_flag: diary_params[:public_flag], impression: diary_params[:impression], impression_public_flag: diary_params[:impression_public_flag], memo: diary_params[:memo], memo_public_flag: diary_params[:memo_public_flag], action: diary_params[:action], action_public_flag: diary_params[:action_public_flag], reco_for: diary_params[:reco_for], reco_for_public_flag: diary_params[:reco_for_public_flag], user_id: current_user.id)
   end
   
+  def destroy
+    diary = Diary.find(params[:id])
+    if diary.user_id == current_user.id
+      diary.destroy
+    end
+  end
+  
   private
   def diary_params
     params.permit(:title, :author, :publisher, :public_flag, :impression, :impression_public_flag, :memo, :memo_public_flag, :action, :action_public_flag, :reco_for, :reco_for_public_flag)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'diaries#index'                   #日記一覧をルートパスに指定する
-  get 'diaries'     => 'diaries#index'   #日記一覧画面（トップページ）
-  get 'diaries/new' => 'diaries#new'     #日記投稿画面
-  post 'diaries'    => 'diaries#create'  #日記投稿機能
-  get 'users/:id'   => 'users#show'      #マイページ画面
+  root 'diaries#index'                        #日記一覧をルートパスに指定する
+  get 'diaries'     => 'diaries#index'        #日記一覧画面（トップページ）
+  get 'diaries/new' => 'diaries#new'          #日記投稿画面
+  post 'diaries'    => 'diaries#create'       #日記投稿機能
+  delete 'diaries/:id' => 'diaries#destroy'   #日記の削除
+  get 'users/:id'   => 'users#show'           #マイページ画面
+
 end
